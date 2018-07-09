bcc71e228234e1e8f8206e967d20e0e9dac6fccf
Briam Santiago <bm.santiago14@gmail.com>
2018-07-09 11:23:37 -0400

    Use API_KEY

--- app/instance-initializers/giphy.js
+++ app/instance-initializers/giphy.js
@@ -1,5 +1,5 @@
 export function initialize(application) {
-  const GIPHY_API_KEY = 'API_KEY';
+  const GIPHY_API_KEY = 'Qo3E5HExMOSygzw52d9VqRJu2wP36xH9';
   const giphy = application.lookup('service:giphy');
 
   giphy.setApiKey(GIPHY_API_KEY);
